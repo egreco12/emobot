a8b1212f0d0843fa362e538e910d8b999f228dc0
Evan Greco <egreco@steelcase.com>
2016-06-19 13:33:12 -0700

    Add basic functionality to send messages to hook url

--- src/emobot/handler.clj
+++ src/emobot/handler.clj
@@ -1,7 +1,19 @@
 (ns emobot.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(def hook-url (System/getenv "SLACK_HOOK_URL"))
+
+(defn post-to-slack
+	"Post given string to our hook"
+	[url msg]
+	(let [m (merge {:username "emobot"
+				   :icon_emoji ":feelsgood:"} msg)]
+	(client/post url {:body (json/write-str m)
+					  :content-type :json})))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
---
+++ src/emobot/links.clj
@@ -0,0 +1,1 @@
+(ns emobot.emo-links)
