961d61dda37dcfcffb6c30d22b1bd1d583a38eac
Evan Greco <egreco@steelcase.com>
2016-06-23 15:44:41 -0700

    Fix auth token and command checks, post youtube links on success

--- src/emobot/handler.clj
+++ src/emobot/handler.clj
@@ -3,6 +3,8 @@
             [compojure.route :as route]
             [clj-http.client :as client]
             [clojure.data.json :as json]
+            [clojure.walk :as walk]
+            [clojure.pprint :refer [pprint]]
             [emobot.links :as links]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :refer [wrap-json-body]]
@@ -20,22 +22,22 @@
   (client/post url {:body (json/write-str m)
             :content-type :json})))
 
-(defn link-to-slack
-  "Post random link to Slack hook url"
+(defn random-link
+  "Grab a random youtube link"
   []
-  (let [e (rand-nth links/links)]
-    (post-to-slack hook-url {:text e})))
+  (rand-nth links/links))
 
 (defroutes app-routes
   (GET "/slack" {:keys [params] :as request}
-        ;;(if (and (= "/slack" (:command params))
-        ;;         (= auth-token (:token params)))
+    (let [p (walk/keywordize-keys params)]
+        (if (and (= "/emobot" (:command p))
+                 (= auth-token (:token p)))
           {:status 200
            :content-type "text/plain"
-           :body "hello, Morrissey!"})
-          ;;{:status 400
-           ;;:content-type "text/plain"
-           ;;:body "Invalid params"})
+           :body (random-link)}
+          {:status 400
+           :content-type "text/plain"
+           :body "Invalid params"})))
   (route/not-found "Not Found"))
 
 (def app
