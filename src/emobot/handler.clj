4a92a2cf50d0a074e05d81bfa17309ca67b422d7
Evan Greco <egreco@steelcase.com>
2016-06-23 16:36:00 -0700

    Respond correctly as a separate response

--- src/emobot/handler.clj
+++ src/emobot/handler.clj
@@ -8,7 +8,8 @@
             [emobot.links :as links]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :refer [wrap-json-body]]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.middleware.params :refer [wrap-params]]
+            [clojure.core.async :refer [thread]]))
 
 (def hook-url (System/getenv "SLACK_HOOK_URL"))
 
@@ -19,25 +20,48 @@
   [url msg]
   (let [m (merge {:username "emobot"
            :icon_emoji ":feelsgood:"} msg)]
+  (println "m: " m)
   (client/post url {:body (json/write-str m)
             :content-type :json})))
+
+(defn vid-to-slack
+  "Post a random video to slack"
+  [url]
+  (post-to-slack url {:text (random-link)}))
 
 (defn random-link
   "Grab a random youtube link"
   []
   (rand-nth links/links))
 
+(def good-response
+  {:status 200
+   :content-type "text/plain"
+   :body "You made me sad..."})
+
+(def bad-response
+  {:status 400
+   :content-type "text/plain"
+   :body "Invalid params"})
+
+(defn build-response
+  "Given text, build a slash command response object"
+  [text status]
+  (json/write-str 
+    {:text text
+     :response_type "in_channel"
+     :status 200
+     :content-type "application/json"}))
+
 (defroutes app-routes
   (GET "/slack" {:keys [params] :as request}
     (let [p (walk/keywordize-keys params)]
         (if (and (= "/emobot" (:command p))
                  (= auth-token (:token p)))
-          {:status 200
-           :content-type "text/plain"
-           :body (random-link)}
-          {:status 400
-           :content-type "text/plain"
-           :body "Invalid params"})))
+        (do 
+          (thread (vid-to-slack (:response_url p)))
+          good-response)
+        bad-response)))
   (route/not-found "Not Found"))
 
 (def app
